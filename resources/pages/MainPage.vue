3dc6cce5c71cb1ced8e78da10e3b2949c32b5d32
MigsGit <miguellgsp12345@gmail.com>
2025-03-28 08:42:46 +0800

    DataTable Installation Debugbar Installation Log Class for Debug Database Connections

--- resources/pages/MainPage.vue
+++ resources/pages/MainPage.vue
@@ -12,59 +12,14 @@
             </ul>
         </div>
     </div>
-
-    <!-- Notices Table -->
-    <div class="container notice-list">
-        <table class="table table-striped table-hover">
-            <thead class="table-primary">
-                <tr>
-                    <th>Number</th>
-                    <th>Title</th>
-                    <th>Registration Date</th>
-                    <th>Views</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>396</td>
-                    <td>[Guide] Installing the ZOOM program and creating an account</td>
-                    <td>2025-03-27</td>
-                    <td>13</td>
-                </tr>
-                <tr>
-                    <td>395</td>
-                    <td>[Notice] Video English classes due to the end of the Skype program</td>
-                    <td>2025-03-13</td>
-                    <td>115</td>
-                </tr>
-                <tr>
-                    <td>394</td>
-                    <td>Information on card company interest-free installment</td>
-                    <td>2025-03-01</td>
-                    <td>115</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-
-    <!-- Pagination -->
-    <div class="container text-center my-4">
-        <nav>
-            <ul class="pagination justify-content-center">
-                <li class="page-item disabled"><a class="page-link" href="#">«</a></li>
-                <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item"><a class="page-link" href="#">4</a></li>
-                <li class="page-item"><a class="page-link" href="#">5</a></li>
-                <li class="page-item"><a class="page-link" href="#">»</a></li>
-            </ul>
-        </nav>
-    </div>
+    <TableAnnouncement/>
     <Footer/>
 </template>
 
 <script setup>
     import Navbar from '../layouts/Navbar.vue';
-     import Footer from '../layouts/Footer.vue';
+    import Footer from '../layouts/Footer.vue';
+    import TableAnnouncement from './TableAnnouncement.vue';
+
+
 </script>
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AnnouncementController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::get('get_announcement_table', [AnnouncementController::class, 'getAnnouncementTable'])->name('announcement.table');
