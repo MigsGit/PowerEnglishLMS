a9799ea5e2b1c632761fc774edb17a3ed347823d
MigsGit <miguellgsp12345@gmail.com>
2025-04-15 13:33:27 +0800

    TODO: Has Many Text Book Classification

--- app/Models/TextBookClassification.php
+++ app/Models/TextBookClassification.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\TextBook;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class TextBookClassification extends Model
 {
-    use HasFactory;
+    protected array $hidden_fields = ['created_at', 'updated_at', 'deleted_at'];
+
+    /**
+     * Get all of the comments for the TextBookClassification
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function text_books()
+    {
+        return $this->hasMany(TextBook::class, 'text_book_classification_id', 'id');
+    }
 }
--- vite.config.js
+++ vite.config.js
@@ -27,7 +27,7 @@ export default defineConfig({
         host: '0.0.0.0',
         port: 5173,
         hmr: {
-          host: 'pixelx', // 👈 Host machine IP/domain
+          host: 'rapidv', // 👈 Host machine IP/domain
           protocol: 'ws',
           port: 5173
         }
